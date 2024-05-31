7fb1868a5be390a4a88bdf80f26701f8311841cd
Ritika Joshi <63815734+RJoshi141@users.noreply.github.com>
2024-05-31 03:42:37 -0400

    logo animation css

---
+++ src/components/LogoAnimation.js
@@ -0,0 +1,22 @@
+// src/components/LogoAnimation.js
+import React from 'react';
+import './LogoAnimation.css'; // Import CSS styles for animation
+
+const LogoAnimation = () => {
+  return (
+    <div className="black-screen">
+      <div className="logo-container">
+        <svg width="500" height="622" viewBox="0 0 500 622" fill="none" xmlns="http://www.w3.org/2000/svg">
+          {/* SVG paths for the R component with drawing animation */}
+          {/* Place the SVG path for the R component here */}
+        </svg>
+        <svg width="482" height="622" viewBox="0 0 482 622" fill="none" xmlns="http://www.w3.org/2000/svg">
+          {/* SVG paths for the J component with drawing animation */}
+          {/* Place the SVG path for the J component here */}
+        </svg>
+      </div>
+    </div>
+  );
+};
+
+export default LogoAnimation;
