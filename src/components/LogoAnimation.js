26966557082f9bb0a272327d485239f469bbec8f
Ritika Joshi <63815734+RJoshi141@users.noreply.github.com>
2024-06-01 13:31:31 -0400

    fadeout works now

--- src/components/LogoAnimation.js
+++ src/components/LogoAnimation.js
@@ -1,25 +1,24 @@
-// LogoAnimation.js
 import React, { useEffect, useState } from 'react';
 import './LogoAnimation.css'; // Import CSS styles for animation
 
 const LogoAnimation = () => {
-  const [animationCompleted, setAnimationCompleted] = useState(false);
+    const [animationCompleted, setAnimationCompleted] = useState(false);
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setAnimationCompleted(true);
-    }, 4000); // Total duration of the animation (in milliseconds)
-    return () => clearTimeout(timer);
-  }, []);
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setAnimationCompleted(true);
+        }, 4000); // Total duration of the animation (in milliseconds)
+        return () => clearTimeout(timer);
+    }, []);
 
-  return (
-    <div className={`black-screen ${animationCompleted ? 'hide' : ''}`}>
-      <div className="logo-container">
-        <img src={`${process.env.PUBLIC_URL}/R.png`} className="logo R-logo" alt="R Logo" />
-        <img src={`${process.env.PUBLIC_URL}/J.png`} className="logo J-logo" alt="J Logo" />
-      </div>
-    </div>
-  );
+    return (
+        <div className="black-screen">
+            <div className={`logo-container ${animationCompleted ? 'hide' : ''}`}>
+                <img src={`${process.env.PUBLIC_URL}/R.png`} className="logo R-logo" alt="R Logo" />
+                <img src={`${process.env.PUBLIC_URL}/J.png`} className="logo J-logo" alt="J Logo" />
+            </div>
+        </div>
+    );
 };
 
 export default LogoAnimation;
