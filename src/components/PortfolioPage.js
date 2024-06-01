004857277f79a22252b0704f8d3f51675d6deb3b
Ritika Joshi <63815734+RJoshi141@users.noreply.github.com>
2024-06-01 15:50:50 -0400

    top left?

--- src/components/PortfolioPage.js
+++ src/components/PortfolioPage.js
@@ -4,19 +4,13 @@ import './PortfolioPage.css'; // Import CSS styles for portfolio page
 const PortfolioPage = () => {
     return (
         <div className="portfolio-container">
+            <img src="/Logo.png" className="top-left-logo" alt="Logo" />
             <div className="intro-container">
                 <div className="profile-picture">
-                    {/* <img src={`${process.env.PUBLIC_URL}/profile-picture.jpg`} alt="Profile" /> */}
+                    {/* Your profile picture goes here */}
                 </div>
                 <div className="intro-text">
-                    <h1>Hi, I'm [Your Name]</h1>
-                    <p>
-                        I'm a passionate [Your Profession] with [Number of Years] years of experience. 
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                        Sed suscipit quam sit amet vestibulum consequat. 
-                        Integer sed risus quis justo facilisis ultrices. 
-                        Etiam interdum, leo vel consectetur mattis, neque ligula ultrices felis, at aliquet odio ipsum non enim.
-                    </p>
+                    {/* Your introduction text goes here */}
                 </div>
             </div>
             {/* Add your buttons and links here */}
