06983173a5a5364b9654df630dee9741cd064e36
Ritika Joshi <63815734+RJoshi141@users.noreply.github.com>
2024-06-02 00:12:19 -0400

    intro works now

--- src/components/PortfolioPage.js
+++ src/components/PortfolioPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './PortfolioPage.css'; // Import CSS styles for portfolio page
+import './PortfolioPage.css';
 
 const PortfolioPage = () => {
     return (
@@ -7,8 +7,8 @@ const PortfolioPage = () => {
             <img src={`${process.env.PUBLIC_URL}/Logo.png`} className="top-left-logo" alt="Logo" />
             <div className="black-screen-portfolio">
                 <div className="intro-container">
-                    <div className="intro-text">
-                        <p>Hi there! My name is</p>
+                    
+                        <p  className="intro-text">Hi there! My name is</p>
                         <br></br>
                         <h1 className="intro-name">Ritika Joshi.</h1>
                         <h1 className="intro-tagline">I'm a Tech-Creative at heart!</h1>
@@ -16,8 +16,8 @@ const PortfolioPage = () => {
                         <p className="intro-tagline1">I'm a recent graduate from the University of Cincinnati with a bachelor's degree in </p>
                         <p className="intro-tagline1">Computer Science, actively seeking full-time opportunities in Front-End Development,  </p>
                         <p className="intro-tagline1">Web Development, and Data Analysis.</p>
-                    </div>
-                    {/* Add your buttons and links here */}
+                    
+                    {/* Add buttons and links here */}
                 </div>
             </div>
         </div>
